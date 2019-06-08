3908264cead02bb1d2beb418b48fc95ad5769613
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2019-06-09 01:35:18 +0200

    Implémentation initiale de la traversée des déclarations

--- header/modfcn.cpp
+++ header/modfcn.cpp
@@ -25,6 +25,7 @@ mod::mod(char const *path):
 		&_dengine,
 		clang::FileSystemOptions()
 	)),
+	_lengine(llvm::EngineBuilder().create()),
 	_generator(clang::CreateLLVMCodeGen(
 		_dengine,
 		"dynlib",
@@ -32,8 +33,7 @@ mod::mod(char const *path):
 		clang::PreprocessorOptions(),
 		clang::CodeGenOptions(),
 		_lcontext
-	)),
-	_lengine(llvm::EngineBuilder().create())
+	))
 	{
 
 	// Prerequisite initialization
@@ -43,6 +43,17 @@ mod::mod(char const *path):
 	// Module interface loading
 	if (!_unit)
 		throw runtime_error("ASTUnit not created");
+
+	if (!_unit->isModuleFile())
+		throw runtime_error("File is not a serialized module");
+
+	// /// Retrieve export declaration
+	auto i(_unit->top_level_begin());
+	auto k(0);
+	while (i != _unit->top_level_end() && (*i)->getKind() != clang::Decl::Kind::Export) { cout << k++ << " "; ++i; }
+	cout << "\n";
+	if (i == _unit->top_level_end())
+		throw runtime_error("File does not export a module interface");
 
 	if (!_lengine)
 		throw runtime_error("JIT Engine not created");
