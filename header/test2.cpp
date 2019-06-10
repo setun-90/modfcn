fbcc72122688413866e3acb0a602f02f3f59699e
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2019-06-10 21:29:54 +0200

    Changer la fonction souhaitée pour être explicite dans le type, on discutera sur cela

--- header/test2.cpp
+++ header/test2.cpp
@@ -8,8 +8,8 @@ int main(void) {
 
 	mod m("./module-2.pcm");
 
-	size_t (*sf)(void)(m.polysym<size_t (*)<long>(void)>("word_sizeof"));
-	cout << "A long is " << sf() << " words on this arch\n";
+	size_t (*sf)(void)(m.polysym<size_t (*)(long)>("word_sizeof"));
+	cout << "A long is " << sf(3) << " words on this arch\n";
 
 	return 0;
 }
